0debd2666189b10549a526a884afe249754e84a7
jeremias <jeremiasprogrammer@gmail.com>
2022-03-14 20:49:55 -0300

    fix: error fixed

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 #define QTD_MESSAGES 2
+#define READ_PIPE 0
+#define WRITE_PIPE 1
 
 int main(){
 	int pipeFileDescriptors[2];
@@ -17,20 +20,26 @@ int main(){
 		printf("Error when create pipe\n");
 		return 1;
 	}
-	for (int i = 0; i < QTD_MESSAGES; i++) {
-		printf("Writing Message %d is %s\n", i, writeMessages[i]);
-		write(pipeFileDescriptors[1], writeMessages[0], sizeof(char) * 20);
-	}
+	
 	pid_t pid;
         pid = fork();
-       	if(pid == 0) {
+       	
+	if(pid == 0) {
+		close(pipeFileDescriptors[READ_PIPE]);
+
 		for (int i = 0; i < QTD_MESSAGES; i++) {
-			read(pipeFileDescriptors[0], readMessages, sizeof(char) * 20);
+			printf("Writing Message %d is %s\n", i, writeMessages[i]);
+			write(pipeFileDescriptors[WRITE_PIPE], writeMessages[i], sizeof(char) * 20);
+		}	
+
+        }else{
+		close(pipeFileDescriptors[WRITE_PIPE]);
+
+		for (int i = 0; i < QTD_MESSAGES; i++) {
+			read(pipeFileDescriptors[READ_PIPE], readMessages, sizeof(char) * 20);
 			printf("Reading Message %d is %s\n", i, readMessages);
         	}
-	
+	}
 
-		exit(0);
-        }
 	return 0;
 }
